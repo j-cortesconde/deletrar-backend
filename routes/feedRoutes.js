3379aaeae6adcaf1a8b543edbd02b8376e681314
j-cortesconde <jcortesconde@gmail.com>
2024-11-05 16:25:10 -0300

    Refactored getUserFeed so it would go on a feedController called through a feedRoute

---
+++ routes/feedRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const FeedController = require('../controllers/feedController');
+const AuthController = require('../controllers/authController');
+
+const router = express.Router();
+const feedController = new FeedController();
+const authController = new AuthController();
+
+router.use(authController.getLoggedInUser);
+
+router.get('/', feedController.getUserFeed);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const collectionRouter = require('./collectionRoutes');
 const commentRouter = require('./commentRoutes');
 const conversationRouter = require('./conversationRoutes');
 const sharedRouter = require('./sharedRoutes');
+const feedRouter = require('./feedRoutes');
 
 const router = express.Router();
 
@@ -14,5 +15,6 @@ router.use('/collections', collectionRouter);
 router.use('/comments', commentRouter);
 router.use('/conversations', conversationRouter);
 router.use('/shareds', sharedRouter);
+router.use('/feed', feedRouter);
 
 module.exports = router;
