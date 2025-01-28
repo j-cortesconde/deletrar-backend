1c31d5494d8e9f11616cefcea47dfef224ff41c4
j-cortesconde <jcortesconde@gmail.com>
2025-01-28 10:04:27 -0300

    Removed unnecessary todos

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-//TODO: Missing FEED, MESSAGE, COLLECTION, COMMENT(this will require nested route on post and on collection)
-//TODO: Missing report (security) features
-//TODO: Missing a global error handler
-//TODO: @frontend: Email url constants (xxxxURL) should be adapted
-// FIXME: I npm i cors and use it here. See if i have to delete (and uninstall it) or not
-//TODO: See CDN for images
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
